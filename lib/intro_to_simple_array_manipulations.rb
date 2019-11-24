81dce1b995bf9829172c55ec7da4a665bb4c5ae0
Marcel Rico <personal.mr4@gmail.com>
2019-11-24 14:39:35 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,29 @@
+def using_push(array,str)
+  
+end
+
+def using_unshift(array,str)
+  
+end
+
+def using_pop(array)
+  
+end
+
+def pop_with_args(array)
+  
+end
+
+def using_shift(array)
+  
+end
+
+def using_concat(array1,array2)
+  
+end
+
+def using_insert(array,element)
+  
+end
+
+def using_flatten()
